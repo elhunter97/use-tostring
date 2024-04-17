1886bb8a55e8e9e4c780e850e6054d46e12d9d36
elhunter97 <hanbaongoc14091271@gmail.com>
2024-04-17 10:04:35 +0700

    use tostring

---
+++ convert_array_to_string.js
@@ -0,0 +1,8 @@
+let arr = ['Red', 'Green', 'White', 'Black'];
+// su dung toString()
+document.write(arr.toString()+'<br>');
+// su dung join
+document.write(arr.join()+'<br>');
+
+document.write(arr.join('+'));
+
